4d4917c6abf786bc3a9ecf3d48375f3f2d6c51d0
Chris Harris <ccharris@gmail.com>
2017-06-06 13:09:52 -0400

    add TOIL_LSF_PROJECT arg

--- src/toil/batchSystems/lsf.py
+++ src/toil/batchSystems/lsf.py
@@ -23,6 +23,7 @@ import subprocess
 import time
 from threading import Thread
 from datetime import date
+import os
 
 # Python 3 compatibility imports
 from six.moves.queue import Empty, Queue
@@ -40,10 +41,13 @@ def prepareBsub(cpu, mem, name):
     if len(str(mem)) >= 10:
         mem = int(mem) / 1000
     logger.debug("Calculated %s cpus requested, %s mem" % (cpu, str(mem/1000000)))
+
     mem = '' if mem is None else '-R "select[type==X86_64 && mem > ' + str(int(mem)/1000000) + '] rusage[mem=' + str(int(mem/1000000)) + ']"'
     cpu = '' if cpu is None else '-n ' + str(int(cpu))
     name = '' if name is None else '-J ' + name.replace(" ","_")
     bsubline = ["bsub", mem, cpu, name, "-cwd", ".", "-o", "/dev/null", "-e", "/dev/null"]
+    if os.environ.get('TOIL_LSF_PROJECT') != None: #then this shit is defined
+        bsubline  = bsubline + ['-P', '"' + os.environ.get('TOIL_LSF_PROJECT')+ '"']
     logger.debug(bsubline)
     return bsubline
 
