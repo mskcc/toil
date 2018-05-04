57c7ba1dc6c2497416766a0f3882af2fab4bc82c
nikhil <nikhilkumar516@gmail.com>
2018-05-04 15:56:18 -0400

    Added in arguments for better logging

--- src/toil/cwl/cwltoil.py
+++ src/toil/cwl/cwltoil.py
@@ -427,7 +427,7 @@ class CWLJob(Job):
 
         self.cwljob = cwljob
         try:
-            self.jobName = " ".join(tool.tool['baseCommand'])
+            self.jobName = " ".join(tool.tool['baseCommand'] + tool.tool['arguments'])
         except KeyError:
             # fall back to the Toil defined class name if the tool doesn't have an identifier
             pass
