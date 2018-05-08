c61b409447d70d9e1a37071641dafaf103505fe1
nikhil <nikhilkumar516@gmail.com>
2018-05-07 21:21:50 -0400

    Use id for job name instead of basecommand

--- src/toil/cwl/cwltoil.py
+++ src/toil/cwl/cwltoil.py
@@ -427,7 +427,7 @@ class CWLJob(Job):
 
         self.cwljob = cwljob
         try:
-            self.jobName = "_".join(tool.tool['baseCommand'] + tool.tool['arguments'])
+            self.jobName = str(tool.tool['id'])
         except KeyError:
             # fall back to the Toil defined class name if the tool doesn't have an identifier
             pass
