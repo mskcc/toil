f14069a6935a696d21939c2254ee4d64c0a7dfc1
nikhil <nikhilkumar516@gmail.com>
2024-01-05 12:01:25 -0500

    Correct package link format

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def run_setup():
     gcs = 'google-cloud-storage==1.6.0'
     gcs_oauth2_boto_plugin = 'gcs_oauth2_boto_plugin==1.14'
     apacheLibcloud = 'apache-libcloud==2.2.1'
-    rdflibJsonld = 'git+https://github.com/mskcc/rdflib-jsonld.git@0.5.1'
+    rdflibJsonld = 'rdflib-jsonld @ git+https://github.com/mskcc/rdflib-jsonld.git@0.5.1'
     cwltool = 'cwltool==3.0.20201203173111'
     galaxyToolUtil = 'galaxy-tool-util'
     htcondor = 'htcondor>=8.6.0'
